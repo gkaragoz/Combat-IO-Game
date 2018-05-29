d519073e731126464266fe924eff9980633c93c5
gkaragoz <annotto3@gmail.com>
2018-05-30 00:04:40 +0300

    Create TestTakeHitEditor script.

---
+++ Combat.io/Assets/Scripts/Test/TestTakeHit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTakeHit : MonoBehaviour {
+
+    public string AnimationTriggerString;
+
+    private Animator _anim;
+
+    private void Awake() {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void TakeHit() {
+        _anim.SetTrigger(AnimationTriggerString);
+    }
+
+}
---
+++ Combat.io/Assets/Scripts/Test/TestTakeHitEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TestTakeHit))]
+public class TestTakeHitEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        TestTakeHit myScript = (TestTakeHit)target;
+        if (GUILayout.Button("Take Hit")) {
+            myScript.TakeHit();
+        }
+    }
+
+}
