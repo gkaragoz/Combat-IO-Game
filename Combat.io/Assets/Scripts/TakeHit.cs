104ba1b99321d13b07937c6d934a11b946f36d96
gkaragoz <annotto3@gmail.com>
2018-05-30 00:16:35 +0300

    Create TakeHit script.

---
+++ Combat.io/Assets/Scripts/TakeHit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeHit : MonoBehaviour {
+
+    public void Action(Rigidbody rb, Vector3 hitPoint) {
+        rb.AddForce(-1 * hitPoint * 100f);
+    }
+}
--- Combat.io/Assets/Scripts/Test/TestTakeHit.cs
+++ Combat.io/Assets/Scripts/Test/TestTakeHit.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(TakeHit), typeof(Rigidbody))]
 public class TestTakeHit : MonoBehaviour {
 
     public string AnimationTriggerString;
 
+    private TakeHit _takeHit;
     private Animator _anim;
+    private Rigidbody _rb;
 
     private void Awake() {
         _anim = GetComponentInChildren<Animator>();
+        _takeHit = GetComponent<TakeHit>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     public void TakeHit() {
         _anim.SetTrigger(AnimationTriggerString);
+        _takeHit.Action(_rb, Vector3.back * 100f);
     }
 
 }
