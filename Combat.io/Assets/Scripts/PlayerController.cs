7481198844eeff0815b8817bb7142ad020651795
gkaragoz <annotto3@gmail.com>
2018-05-29 17:37:35 +0300

    Create PlayerController.cs script and Player prefab.

---
+++ Combat.io/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 5f;  // Player base movement speed.
+
+    private Transform _mesh;          // Player's character which has selected Transform object.
+
+    private void Awake() {
+        FindMesh();
+    }
+
+	void Update () {
+		if (HasMesh()) {
+            Move();
+        }
+	}
+
+    // Player's abilities like movement, attack, die etc.
+    #region Abilities
+    private void Move() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+    #endregion
+
+    // Init functions like find meshes, has mesh etc.
+    #region Initializes
+    private void FindMesh() {
+        _mesh = transform.Find("Mesh").GetChild(0);
+    }
+
+    private bool HasMesh() {
+        return (_mesh != null) ? true : false;
+    }
+    #endregion
+}
