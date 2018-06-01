272f67b1674b1bce895bdabf25dfcb58181d08d2
gkaragoz <annotto3@gmail.com>
2018-06-01 23:03:30 +0300

    Create TestMeleeAttack and TestMeleeAttackEditor scripts. Implement it to Bots.

---
+++ Combat.io/Assets/Scripts/Test/TestMeleeAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMeleeAttack : MonoBehaviour {
+
+    public string animationKeyString;
+
+    private Animator _anim;
+
+    private void Awake() {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Action() {
+        _anim.SetTrigger(animationKeyString);
+    }
+}
---
+++ Combat.io/Assets/Scripts/Test/TestMeleeAttackEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TestMeleeAttack))]
+public class TestMeleeAttackEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        TestMeleeAttack myScript = (TestMeleeAttack)target;
+        if (GUILayout.Button("Play/Stop Run")) {
+            myScript.Action();
+        }
+    }
+}
